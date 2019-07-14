945f830207904b1ba06b277d42975fdcc82b3a85
Kashif Rasul <kashif.rasul@zalando.de>
2019-07-14 14:34:41 +0200

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytorch-ts',
+    version='0.1.0',
+    description="PyTorch Probabilistic Time Series Modeling framework",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+
+    url='https://github.com/kashif/pytorch-ts',
+    license='Apache License Version 2.0',
+
+    packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
+    zip_safe=True,
+    python_requires=">=3.6",
+    install_requires = [
+        'torch>=1.0.0',
+        'holidays',
+        'numpy',
+        'pandas',
+        'tqdm',
+        'ujson',
+        'pydantic',
+    ],
+
+    test_suite='tests',
+    tests_require = [
+        'flake8',
+        'pytest'
+    ],
+)
