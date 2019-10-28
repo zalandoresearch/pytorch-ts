52ed9d893748b6acb28fdd909b4ad411f9c00196
Dr. Kashif Rasul <kashif.rasul@gmail.com>
2019-10-28 14:18:16 +0100

    fixed lambda

--- pts/modules/lambda.py
+++ pts/modules/lambda.py
@@ -2,10 +2,9 @@ import torch
 import torch.nn as nn
 
 class Lambda(nn.Module):
-    def __init(self, function):
+    def __init__(self, function):
         super().__init__()
-        func_dict = {sym: getattr(sym, function), Tensor: getattr(torch.Tensor, function)}
-        self._func = lambda *args: func_dict(*args)
+        self._func = function
     
     def forward(self, x, *args):
         return self._func(x, *args)
