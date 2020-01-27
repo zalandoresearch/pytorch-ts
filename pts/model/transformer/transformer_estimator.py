3184f5d588bb1660913806e9b3dcfe0a7fc4ceec
Dr. Kashif Rasul <kashif.rasul@gmail.com>
2020-01-27 16:24:02 +0100

    initial transformer model
    todo decoder encoder

---
+++ pts/model/transformer/transformer_estimator.py
@@ -0,0 +1,213 @@
+from typing import List, Optional
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+
+from pts import Trainer
+from pts.model import PTSEstimator, PTSPredictor, copy_parameters
+from pts.modules import RealNVP
+from pts.dataset import FieldName
+from pts.transform import (
+    Transformation,
+    Chain,
+    InstanceSplitter,
+    ExpectedNumInstanceSampler,
+    CDFtoGaussianTransform,
+    cdf_to_gaussian_forward_transform,
+    RenameFields,
+    AsNumpyArray,
+    ExpandDimArray,
+    AddObservedValuesIndicator,
+    AddTimeFeatures,
+    VstackFeatures,
+    SetFieldIfNotPresent,
+    TargetDimIndicator,
+)
+from pts.feature import (
+    TimeFeature,
+    fourier_time_features_from_frequency_str,
+    get_fourier_lags_for_frequency,
+)
+
+from .transformer_decoder import TransformerDecoder
+from .transfomer_encoder import TransformerEncoder
+from .transfomer_network import TransformerTrainingNetwork, TransformerPredictionNetwork
+
+class TransformerEstimator(PTSEstimator):
+    def __init__(
+        self,
+        freq: str,
+        prediction_length: int,
+        context_length: Optional[int] = None,
+        trainer: Trainer = Trainer(),
+        dropout_rate: float = 0.1,
+        cardinality: Optional[List[int]] = None,
+        embedding_dimension: int = 20,
+        distr_output: DistributionOutput = StudentTOutput(),
+        model_dim: int = 32,
+        inner_ff_dim_scale: int = 4,
+        pre_seq: str = "dn",
+        post_seq: str = "drn",
+        act_type: str = "softrelu",
+        num_heads: int = 8,
+        scaling: bool = True,
+        lags_seq: Optional[List[int]] = None,
+        time_features: Optional[List[TimeFeature]] = None,
+        use_feat_dynamic_real: bool = False,
+        use_feat_static_cat: bool = False,
+        num_parallel_samples: int = 100,
+    ) -> None:
+        super().__init__(trainer=trainer)
+
+        self.freq = freq
+        self.prediction_length = prediction_length
+        self.context_length = (
+            context_length if context_length is not None else prediction_length
+        )
+        self.distr_output = distr_output
+        self.dropout_rate = dropout_rate
+        self.use_feat_dynamic_real = use_feat_dynamic_real
+        self.use_feat_static_cat = use_feat_static_cat
+        self.cardinality = cardinality if use_feat_static_cat else [1]
+        self.embedding_dimension = embedding_dimension
+        self.num_parallel_samples = num_parallel_samples
+        self.lags_seq = (
+            lags_seq if lags_seq is not None else get_lags_for_frequency(freq_str=freq)
+        )
+        self.time_features = (
+            time_features
+            if time_features is not None
+            else time_features_from_frequency_str(self.freq)
+        )
+        self.history_length = self.context_length + max(self.lags_seq)
+        self.scaling = scaling
+
+        self.config = {
+            "model_dim": model_dim,
+            "pre_seq": pre_seq,
+            "post_seq": post_seq,
+            "dropout_rate": dropout_rate,
+            "inner_ff_dim_scale": inner_ff_dim_scale,
+            "act_type": act_type,
+            "num_heads": num_heads,
+        }
+
+        self.encoder = TransformerEncoder(
+            self.context_length, self.config, prefix="enc_"
+        )
+        self.decoder = TransformerDecoder(
+            self.prediction_length, self.config, prefix="dec_"
+        )
+
+    def create_transformation(self) -> Transformation:
+        remove_field_names = [
+            FieldName.FEAT_DYNAMIC_CAT,
+            FieldName.FEAT_STATIC_REAL,
+        ]
+        if not self.use_feat_dynamic_real:
+            remove_field_names.append(FieldName.FEAT_DYNAMIC_REAL)
+
+        return Chain(
+            [RemoveFields(field_names=remove_field_names)]
+            + (
+                [SetField(output_field=FieldName.FEAT_STATIC_CAT, value=[0.0])]
+                if not self.use_feat_static_cat
+                else []
+            )
+            + [
+                AsNumpyArray(field=FieldName.FEAT_STATIC_CAT, expected_ndim=1),
+                AsNumpyArray(
+                    field=FieldName.TARGET,
+                    # in the following line, we add 1 for the time dimension
+                    expected_ndim=1 + len(self.distr_output.event_shape),
+                ),
+                AddObservedValuesIndicator(
+                    target_field=FieldName.TARGET,
+                    output_field=FieldName.OBSERVED_VALUES,
+                ),
+                AddTimeFeatures(
+                    start_field=FieldName.START,
+                    target_field=FieldName.TARGET,
+                    output_field=FieldName.FEAT_TIME,
+                    time_features=self.time_features,
+                    pred_length=self.prediction_length,
+                ),
+                AddAgeFeature(
+                    target_field=FieldName.TARGET,
+                    output_field=FieldName.FEAT_AGE,
+                    pred_length=self.prediction_length,
+                    log_scale=True,
+                ),
+                VstackFeatures(
+                    output_field=FieldName.FEAT_TIME,
+                    input_fields=[FieldName.FEAT_TIME, FieldName.FEAT_AGE]
+                    + (
+                        [FieldName.FEAT_DYNAMIC_REAL]
+                        if self.use_feat_dynamic_real
+                        else []
+                    ),
+                ),
+                InstanceSplitter(
+                    target_field=FieldName.TARGET,
+                    is_pad_field=FieldName.IS_PAD,
+                    start_field=FieldName.START,
+                    forecast_start_field=FieldName.FORECAST_START,
+                    train_sampler=ExpectedNumInstanceSampler(num_instances=1),
+                    past_length=self.history_length,
+                    future_length=self.prediction_length,
+                    time_series_fields=[
+                        FieldName.FEAT_TIME,
+                        FieldName.OBSERVED_VALUES,
+                    ],
+                ),
+            ]
+        )
+
+    def create_training_network(self, device: torch.device) -> TransformerTrainingNetwork:
+
+        training_network = TransformerTrainingNetwork(
+            encoder=self.encoder,
+            decoder=self.decoder,
+            history_length=self.history_length,
+            context_length=self.context_length,
+            prediction_length=self.prediction_length,
+            distr_output=self.distr_output,
+            cardinality=self.cardinality,
+            embedding_dimension=self.embedding_dimension,
+            lags_seq=self.lags_seq,
+            scaling=True,
+        ).to(device)
+
+        return training_network
+
+    def create_predictor(
+        self, transformation: Transformation, trained_network: nn.Module,  device: torch.device,
+    ) -> Predictor:
+
+        prediction_network = TransformerPredictionNetwork(
+            encoder=self.encoder,
+            decoder=self.decoder,
+            history_length=self.history_length,
+            context_length=self.context_length,
+            prediction_length=self.prediction_length,
+            distr_output=self.distr_output,
+            cardinality=self.cardinality,
+            embedding_dimension=self.embedding_dimension,
+            lags_seq=self.lags_seq,
+            scaling=True,
+            num_parallel_samples=self.num_parallel_samples,
+        )
+
+        copy_parameters(trained_network, prediction_network)
+
+        return PTSPredictor(
+            input_transform=transformation,
+            prediction_net=prediction_network,
+            batch_size=self.trainer.batch_size,
+            freq=self.freq,
+            prediction_length=self.prediction_length,
+            device=device,
+            output_transform=None,
+        )
