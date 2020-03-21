fix scaling of alpha

pts/modules/distribution_output.py
@@ -124,7 +124,7 @@ class NegativeBinomialOutput(DistributionOutput):
 
         if scale is not None:
             mu *= scale
-            alpha *= torch.sqrt(scale + 1.0)
+            alpha /= torch.sqrt(scale)
 
         n = 1.0 / alpha
         p = mu * alpha / (1.0 + mu * alpha)