289151556bf5616857ba6892afb74e052dbbe9b2
Dr. Kashif Rasul <kashif.rasul@gmail.com>
2020-07-13 12:12:56 +0200

    fix import

--- pts/distributions/__init__.py
+++ pts/distributions/__init__.py
@@ -1,2 +1,6 @@
 from .utils import broadcast_shape
-from .zero_inflated import ZeroInflatedDistribution, ZeroInflatedPoisson
+from .zero_inflated import (
+    ZeroInflatedDistribution,
+    ZeroInflatedPoisson,
+    ZeroInflatedNegativeBinomial,
+)
