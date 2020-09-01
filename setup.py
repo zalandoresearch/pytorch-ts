262d40026a8d49a9a033e9027875179bb56403b4
Dr. Kashif Rasul <kashif.rasul@gmail.com>
2020-09-01 15:01:07 +0200

    v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytorchts',
-    version='0.1.1',
+    version='0.2.0',
     description="PyTorch Probabilistic Time Series Modeling framework",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
