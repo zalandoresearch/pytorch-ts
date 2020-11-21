6bed91f6b2b97a6ed84f120f5a5c2f732b72fb33
Kashif Rasul <kashif.rasul@gmail.com>
2020-11-21 11:30:50 +0100

    initial uncond image gaussian diff
    TODO make it work for multivariate vector
    add conditioning

--- pts/modules/__init__.py
+++ pts/modules/__init__.py
@@ -22,3 +22,4 @@ from .feature import FeatureEmbedder, FeatureAssembler
 from .flows import RealNVP, MAF
 from .lambda_layer import LambdaLayer
 from .scaler import MeanScaler, NOPScaler
+from .gaussian_diffusion import GaussianDiffusion
