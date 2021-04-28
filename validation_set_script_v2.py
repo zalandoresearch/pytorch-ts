ce71ee9429cad427fed0dfc1981ec0f6326a2427
larkz <liu.larkin@gmail.com>
2021-04-28 16:31:18 +0200

    updated working inference

--- validation_set_script_v2.py
+++ validation_set_script_v2.py
@@ -11,6 +11,9 @@ from pts import Trainer
 import copy
 from datetime import datetime, timedelta
 
+import boto3
+from io import BytesIO
+import pickle as pkl
 
 freq = '1D'
 
@@ -26,6 +29,7 @@ hyperparameters = {
     "context_length": str(context_length),
     "prediction_length": str(prediction_length),
     "test_quantiles": [0.16, 0.5, 0.84],
+    # "test_quantiles": [0.01, 0.5, 0.99],
 }
 
 TEST_QUANTILES_STR = '["0.16", "0.5", "0.84"]'
@@ -41,7 +45,7 @@ def get_upper_lower_qt(forecasts, upper_qt = UPPER_QT, lower_qt = LOWER_QT):
     mean = np.array(list(map(lambda x: get_quantile(x.samples, 0.5), forecasts)))
     return mean, lower_forecast_qt, upper_forecast_qt
 
-def get_time_window_for_forecast_pts(forward_index, json_data, context_length, json_data_train, forecast_len):
+def get_time_window_for_forecast_pts(forward_index, json_data, json_data_train, context_length, forecast_len):
     # Censor even number groups
     new_json_data = copy.deepcopy(json_data)
     original_split_date = datetime.strptime(json_data[0]['start'], "%Y-%m-%d")
@@ -49,16 +53,19 @@ def get_time_window_for_forecast_pts(forward_index, json_data, context_length, j
     if forward_index >= 0:
         for i in range(len(json_data)):
             past_start_date = original_split_date - timedelta(days=(context_length - forward_index))
-            new_json_data[i]['target'] = json_data_train[i]['target'][-(context_length - forward_index):] + json_data[i]['target'][:forward_index]
-            json_dynam_feat_train = np.array(json_data_train[i]['dynamic_feat'])[:, -(context_length - forward_index):]
-            json_dynam_feat_test = np.array(json_data[i]['dynamic_feat'])[:, :forward_index+forecast_len]
-            new_json_data[i]['dynamic_feat'] = np.concatenate((json_dynam_feat_train, json_dynam_feat_test), axis=1).tolist()
+            data_train_sample = json_data_train[i]['target'][-(context_length - forward_index):]
+            data_test_samnple = json_data[i]['target'][:forward_index]
+
+            new_json_data[i]['target'] = np.concatenate([ json_data_train[i]['target'][-(context_length - forward_index):] , json_data[i]['target'][:forward_index] ])
+            json_dynam_feat_train = np.array(json_data_train[i]['feat_dynamic_real'])[:, -(context_length - forward_index):]
+            json_dynam_feat_test = np.array(json_data[i]['feat_dynamic_real'])[:, :forward_index+forecast_len]
+            new_json_data[i]['feat_dynamic_real'] = np.concatenate((json_dynam_feat_train, json_dynam_feat_test), axis=1).tolist()
             new_json_data[i]['start'] = str(past_start_date)
     else:
         for i in range(len(json_data)):            
             past_start_date = original_split_date + timedelta(days=forward_index)
             new_json_data[i]['target'] = json_data_train[i]['target'][forward_index-context_length:forward_index]
-            new_json_data[i]['dynamic_feat'] = np.array(json_data_train[i]['dynamic_feat'])[:, forward_index-context_length:forward_index].tolist()
+            new_json_data[i]['feat_dynamic_real'] = np.array(json_data_train[i]['feat_dynamic_real'])[:, forward_index-context_length:forward_index].tolist()
             new_json_data[i]['start'] = str(past_start_date)
     return new_json_data
 
@@ -71,8 +78,9 @@ def rolling_inference_pts(predictor, train_data, test_data, n_forward):
     upper_forecast_qt_ts = np.zeros((n_dim, n_forward))
 
     for f in range(n_forward):
-        window_instances = get_time_window_for_forecast_pts(f, test_data, train_data, f)
-        window_infernce_data = ListDataset(list(filter(lambda x: {"start": x["start"], "target": x["target"]}, window_instances)), freq = "1D")
+        window_instances = get_time_window_for_forecast_pts(f, test_data, train_data, 5, 4)
+        window_infernce_data = ListDataset(window_instances, freq = "1D")
+        # window_infernce_data = ListDataset(list(filter(lambda x: {"start": x["start"], "target": x["target"]}, window_instances)), freq = "1D")
         mean, lower_forecast_qt, upper_forecast_qt = get_upper_lower_qt(list(predictor.predict(window_infernce_data)))
 
         mean_ts[:, f] = mean[:, :1].reshape(-1)# .flatten()
@@ -80,3 +88,43 @@ def rolling_inference_pts(predictor, train_data, test_data, n_forward):
         upper_forecast_qt_ts[:, f] = upper_forecast_qt[:, :1].reshape(-1)# .flatten()
 
     return mean_ts, lower_forecast_qt_ts, upper_forecast_qt_ts
+
+
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    special_id = "split-id-376582_de_2019-01-07_ooh_placeholder_ONOFF-dates-2019-01-07_2020-01-08_2020-01-22"
+    predictor = torch.load(f"inference_output/predictor_obj_{special_id}.pt", map_location=torch.device('cpu'))
+    predictor.device = 'cpu'
+    
+    bucket = 'ab-testing-dpa'
+    training_filename = f"pytorch-ts-format/pytorch_ts-training_data-{special_id}.pkl"
+    test_filename = f"pytorch-ts-format/pytorch_ts-test_data-{special_id}.pkl"
+
+    s3 = boto3.resource('s3')
+    with BytesIO() as data:
+        s3.Bucket(bucket).download_fileobj(training_filename, data)
+        data.seek(0) 
+        training_data = pkl.load(data)
+
+    with BytesIO() as data:
+        s3.Bucket(bucket).download_fileobj(test_filename, data)
+        data.seek(0) 
+        test_data = pkl.load(data)
+
+    forecast_len = 4
+    context_length = 3
+    forward_index = 0
+
+    time_window = get_time_window_for_forecast_pts(forward_index, test_data, training_data, context_length, forecast_len)
+    
+    # time_window_input = ListDataset(time_window, freq = "1D")
+    time_window_input = ListDataset(test_data, freq = "1D")
+
+    sample_prediction = list(predictor.predict(time_window_input))
+
+    n_forward = 14
+    mean_ts, lower_forecast_qt_ts, upper_forecast_qt_ts = rolling_inference_pts(predictor, training_data, test_data, n_forward)
+
+    print("testing done")
+
