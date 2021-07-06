8fbec1ffa170dc3759e9a12e9c0f7fee304914ab
Dr. Kashif Rasul <kashif.rasul@gmail.com>
2021-07-06 13:18:07 +0200

    version 0.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pytorchts",
-    version="0.4.0",
+    version="0.5.0",
     description="PyTorch Probabilistic Time Series Modeling framework",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
