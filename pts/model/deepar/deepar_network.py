e7f53659c3c94c0a64303136c565f588c8b52227
Kashif Rasul <kashif.rasul@gmail.com>
2021-09-24 11:45:35 +0200

    added moving avg features

--- pts/model/deepar/deepar_network.py
+++ pts/model/deepar/deepar_network.py
@@ -34,6 +34,7 @@ class DeepARNetwork(nn.Module):
         cardinality: List[int],
         embedding_dimension: List[int],
         lags_seq: List[int],
+        agg_lags: Optional[List[int]] = None,
         scaling: bool = True,
         dtype: np.dtype = np.float32,
     ) -> None:
@@ -52,6 +53,7 @@ class DeepARNetwork(nn.Module):
         self.dtype = dtype
 
         self.lags_seq = lags_seq
+        self.agg_lags = agg_lags
 
         self.distr_output = distr_output
         rnn = {"LSTM": nn.LSTM, "GRU": nn.GRU}[self.cell_type]
@@ -116,6 +118,31 @@ class DeepARNetwork(nn.Module):
             lagged_values.append(sequence[:, begin_index:end_index, ...])
         return torch.stack(lagged_values, dim=-1)
 
+    @staticmethod
+    def get_mean_agg_lags(
+        sequence: torch.Tensor,  # [B, T, 1]
+        agg_lags: List[int],
+        past_observed_values: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        accumlated_sum = torch.cumsum(sequence, dim=-1)
+        averaged_seqs = []
+
+        for w in agg_lags:
+            moving_avg = torch.zeros_like(accumlated_sum)
+            moving_avg[...] = np.nan
+            moving_avg[:, w:, ...] = (
+                accumlated_sum[:, w:, ...] - accumlated_sum[:, :-w, ...]
+            )
+            moving_avg /= w
+
+            averaged_seqs.append(moving_avg)
+
+        if past_observed_values is not None:
+            # set all starting values to unobserved
+            past_observed_values[..., : max(agg_lags)] = 0.0
+
+        return torch.stack(averaged_seqs, dim=-1)
+
     def unroll_encoder(
         self,
         feat_static_cat: torch.Tensor,  # (batch_size, num_features)
@@ -157,6 +184,13 @@ class DeepARNetwork(nn.Module):
             subsequences_length=subsequences_length,
         )
 
+        if self.agg_lags is not None:
+            moving_avg = self.get_mean_agg_lags(
+                sequence=sequence,
+                agg_lags=self.agg_lags,
+                past_observed_values=past_observed_values,
+            )
+
         # scale is computed on the context length last units of the past target
         # scale shape is (batch_size, 1, *target_shape)
         _, scale = self.scaler(
@@ -194,7 +228,17 @@ class DeepARNetwork(nn.Module):
         )
 
         # (batch_size, sub_seq_len, input_dim)
-        inputs = torch.cat((input_lags, time_feat, repeated_static_feat), dim=-1)
+        inputs = torch.cat(
+            (
+                input_lags,
+                time_feat,
+                moving_avg[..., -subsequences_length:, :] / scale.unsqueeze(-1)
+                if self.agg_lags is not None
+                else None,
+                repeated_static_feat,
+            ),
+            dim=-1,
+        )
 
         # unroll encoder
         outputs, state = self.rnn(inputs)
@@ -370,6 +414,14 @@ class DeepARPredictionNetwork(DeepARNetwork):
             # (batch_size * num_samples, 1, *target_shape, num_lags)
             lags_scaled = lags / repeated_scale.unsqueeze(-1)
 
+            if self.agg_lags is not None:
+                moving_avg = self.get_mean_agg_lags(
+                    sequence=repeated_past_target,
+                    agg_lags=self.agg_lags,
+                )
+                moving_avg = moving_avg / repeated_scale.unsqueeze(-1)
+
+
             # from (batch_size * num_samples, 1, *target_shape, num_lags)
             # to (batch_size * num_samples, 1, prod(target_shape) * num_lags)
             input_lags = lags_scaled.reshape(
@@ -378,7 +430,12 @@ class DeepARPredictionNetwork(DeepARNetwork):
 
             # (batch_size * num_samples, 1, prod(target_shape) * num_lags + num_time_features + num_static_features)
             decoder_input = torch.cat(
-                (input_lags, repeated_time_feat[:, k : k + 1, :], repeated_static_feat),
+                (
+                    input_lags,
+                    repeated_time_feat[:, k : k + 1, :],
+                    moving_avg[:, -1:, :] if self.agg_lags is not None else None,
+                    repeated_static_feat,
+                ),
                 dim=-1,
             )
 
